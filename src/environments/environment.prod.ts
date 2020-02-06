1279877b063778e5e3b39526bcd996900d6cf948
zhonghuiquan@cmcm.com <zhonghuiquan@cmcm.com>
2020-02-06 16:12:43 +0800

    更新正式版配置

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
-  walletUrl: '{WALLET_URL}',
-  votingUrl: '{VOTING_URL}',
-  appName: '{APP_NAME}',
-  logoUrl: '{LOGO_URL}',
-  blockchainUrl: '{BLOCKCHAIN_URL}',
-  chainId: '{CHAIN_ID}',
+  walletUrl: 'http://iyas.top',
+  votingUrl: 'http://iyas.top',
+  appName: 'YAS Tracker',
+  logoUrl: '/assets/logo.png',
+  blockchainUrl: 'http://45.77.40.115:8888',
+  chainId: 'EOS7oB3Ruwavbvja9SQpN58iK2zK8W737E6hvUwXUUj9iQ5a49Tff',
   showAds: false,
-  tokensUrl: '{TOKENS_URL}',
-  tickerUrl: '{TICKER_URL}',
-  token: '{TOKEN}'
+  tokensUrl: 'https://raw.githubusercontent.com/eoscafe/eos-airdrops/master/tokens.json',
+  tickerUrl: 'https://api.coinmarketcap.com/v2/ticker/1765/',
+  token: 'YAS'
 };
